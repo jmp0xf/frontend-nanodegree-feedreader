0a52ed85101e7467981460e44d5aa76db239a4a2
jmp0xf <jmp0xf@gmail.com>
2017-12-29 16:44:54 +0800

    test: 增加对  Feeds URL 格式的检查

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -11,6 +11,9 @@ $(function() {
      * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
     */
     describe('RSS Feeds', function() {
+        // URL 正则表达式
+        var regularExpressionUrl = /^((ht|f)tps?):\/\/([\w\-]+(\.[\w\-]+)*\/)*[\w\-]+(\.[\w\-]+)*\/?(\?([\w\-\.,@?^=%&:\/~\+#]*)+)?/;
+
         /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
          * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
          * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
@@ -25,11 +28,13 @@ $(function() {
         /*
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
-        it('url is defined', function() {
+        it('url is defined properly', function() {
             allFeeds.forEach(function (feed) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
+                // 检查 URL 格式是否正确
+                expect(feed.url).toMatch(regularExpressionUrl);
             });
         });
 
